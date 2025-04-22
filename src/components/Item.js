60a8061c91c22765381233ee62a55f089075207e
Haron M Maina <haronmmaina@Harons-MacBook-Air.local>
2025-04-22 11:28:03 +0300

    Builds the app

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,21 @@
+import { useState } from "react";
 import React from "react";
 
 function Item({ name, category }) {
+const[cart, addCart]=useState(false)
+const toggleCart = ()=>{
+  addCart(prevValue=>!prevValue)
+}
+
+const Whatcart = cart ? "in-cart":""
+
+
+
   return (
-    <li className="">
+    <li className={Whatcart}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={toggleCart}>{cart? "Remove From Cart":"Add to Cart"}</button>
     </li>
   );
 }
